1bda44815d724241ee00f97232ad2b03c70d8223
marion dieudonné <m.j.dieudonne@orange.fr>
2022-02-03 23:17:47 +0100

    second commit - v2

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ const alert = {
   border : "4px solid #a72929",
   padding : "3rem",
   textAlign : "center",
+  color : "red",
 }
 
 window.timer = {
@@ -30,29 +31,33 @@ class Demo extends React.Component {
       count:150, 
       sequence: "travail", 
       styles : styles1,  
-      options : false, 
+      options : false,
+      launch : false, 
       inputWork : 150, 
       inputPause :100,
       display : "",
     };
   }
   
-
   switchSequence = (sequence) => {
     switch(sequence) {
       case "pause" : 
         return {count : this.state.inputWork, sequence: "travail", styles : styles1};
       case "travail" : 
         return {count : this.state.inputPause, sequence: "pause", styles : styles2};
+      default : 
+      return null;
     }
   }
 
   setOptions = () => {
     this.setState({options : true});
+    this.setState({launch : false}); 
   }
 
   validateOptions = () => {
     this.setState({options : false});
+    this.resetT();
   }
 
   setTimeWork = (e) => {
@@ -68,9 +73,9 @@ class Demo extends React.Component {
       this.createTimer(this.state.count);
       if(this.state.count <= 0) {
         clearInterval(window.timer.time);
-        this.reset();
+        this.launch();
       }
-      if(this.state.count <= 20){
+      else if(this.state.count <= 20){
         this.setState({styles : alert});
       }
     });
@@ -83,11 +88,17 @@ class Demo extends React.Component {
     this.setState({display : stringTimer});
   }
 
-  reset = () => {
+  launch = () => {
     this.setState(
       this.switchSequence(this.state.sequence), () => {
         this.timer();
     });
+    this.setState({launch : true}); 
+  }
+
+  resetT = () => {
+    this.stop();
+    this.launch();
   }
 
   stop = () => {
@@ -105,24 +116,32 @@ class Demo extends React.Component {
   }
 
   render(){
-    if(this.state.options === false){
+    if(this.state.options === false && this.state.launch === false){
       return (
         <div style={this.state.styles}>
-          <button onClick={this.reset}> Commencer </button>
+          <h1> {this.state.sequence} </h1>
+          <h2> {this.state.display} </h2>
+          <button onClick={this.launch}> Commencer </button>
+          <button onClick={this.setOptions}> Choisir les options </button>
+        </div>
+      )
+    }
+    else if (this.state.options === false && this.state.launch === true){
+      return (
+        <div style={this.state.styles}>
+          <h1> {this.state.sequence} </h1>
+          <h2> {this.state.display} </h2>
           <button onClick={this.stop}> Arrêter </button>
           <button onClick={this.relaunch}> Relancer </button>
           <button onClick={this.setOptions}> Choisir les options </button>
-          <h1> {this.state.sequence} | {this.state.display} </h1>
         </div>
       )
     }
     else {
       return (
         <div style={this.state.styles}>
-          <button onClick={this.reset}> Commencer </button>
-          <button onClick={this.stop}> Arrêter </button>
-          <button onClick={this.relaunch}> Relancer </button>
-          <button onClick={this.setOptions}> Choisir les options </button>
+          <h1> {this.state.sequence} </h1>
+          <h2> {this.state.display} </h2>
           <div>
             <label htmlFor="work"> work : </label>
             <input id="work" value={this.state.inputWork} onChange={this.setTimeWork} placeholder="temps de travail"/>
@@ -130,7 +149,6 @@ class Demo extends React.Component {
             <input id="pause" value={this.state.inputPause} onChange={this.setTimePause} placeholder="temps de pause"/>
             <button onClick={this.validateOptions}>Valider</button>
             </div>  
-          <h1> {this.state.sequence} | {this.state.display} </h1>
         </div>
       )
     }
